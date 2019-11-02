2d92d269d50c8dc87357cc78e5f3edfbff21bd82
KristinHannah <kristinhannahk@gmail.com>
2019-11-02 14:38:31 -0400

    working on UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,14 @@ class UsersController < ApplicationController
     end 
 
     def create 
+        @user = User.new 
+        @user.email = params[:user][:email]
+        @user.save 
+    end 
 
+    private 
+
+    def user_params(params)
+        params.require(:user).permit(:email, :password_digest)
     end 
 end
