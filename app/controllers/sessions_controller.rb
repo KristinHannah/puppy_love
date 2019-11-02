3dddc52c8caf720b6cff24a95bf4d8502f3f3755
KristinHannah <kristinhannahk@gmail.com>
2019-11-02 16:27:22 -0400

    working on sessions controller for facebook login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,13 @@ class SessionsController < ApplicationController
 
         if auth_hash = request.env["omniauth.auth"]
             #logged in via facebook
+            oauth_email = request.env["omniauth.auth"]["email"]
+            if user = User.find_by(:email => oauth_email)
+                session[:user_id] = user.id
+            else 
+                user = User.create(:email => oauth_email)
+                session[:user_id] = user.id
+            end 
         else 
             #logged in normal
          user = User.find_by(:email => params[:email])
