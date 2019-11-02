4461d3ad26136a6f1d00f08bbfb0c517ea592721
KristinHannah <kristinhannahk@gmail.com>
2019-11-02 14:23:41 -0400

    stubbing out users controller and sign up view

--- app/models/dog_breed.rb
+++ app/models/dog_breed.rb
@@ -1,4 +1,7 @@
+require "pry"
 class DogBreed < ApplicationRecord
     has_many :matches 
     has_many :users, through: :matches 
+    belongs_to :atts_table
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :matches
     has_many :dog_breeds, through: :matches 
- 
+    belongs_to :atts_table
 end
