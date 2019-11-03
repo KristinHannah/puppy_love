9aa02aaceddd9635f166945dda4037dcc75c85f1
KristinHannah <kristinhannahk@gmail.com>
2019-11-03 12:38:56 -0500

    setting up seed data

---
+++ db/seeds.rb
@@ -0,0 +1,65 @@
+
+dog_1 = DogBreed.create(name: , origins: , life_span: , group: )
+atts_1 = AttsTable.create(dog_breed_id: dog_1.id, apartment_friendly: , hypoallergenic: pet_friendly: , noise: )
+dog_1.atts_table_id = atts_1.id 
+dog_1.save
+
+dog_1 = DogBreed.create(name: , origins: , life_span: , group: )
+atts_1 = AttsTable.create(dog_breed_id: dog_1.id, apartment_friendly: , hypoallergenic: pet_friendly: , noise: )
+dog_1.atts_table_id = atts_1.id 
+dog_1.save
+
+dog_1 = DogBreed.create(name: , origins: , life_span: , group: )
+atts_1 = AttsTable.create(dog_breed_id: dog_1.id, apartment_friendly: , hypoallergenic: pet_friendly: , noise: )
+dog_1.atts_table_id = atts_1.id 
+dog_1.save
+
+dog_1 = DogBreed.create(name: , origins: , life_span: , group: )
+atts_1 = AttsTable.create(dog_breed_id: dog_1.id, apartment_friendly: , hypoallergenic: pet_friendly: , noise: )
+dog_1.atts_table_id = atts_1.id 
+dog_1.save
+
+dog_1 = DogBreed.create(name: , origins: , life_span: , group: )
+atts_1 = AttsTable.create(dog_breed_id: dog_1.id, apartment_friendly: , hypoallergenic: pet_friendly: , noise: )
+dog_1.atts_table_id = atts_1.id 
+dog_1.save
+
+dog_1 = DogBreed.create(name: , origins: , life_span: , group: )
+atts_1 = AttsTable.create(dog_breed_id: dog_1.id, apartment_friendly: , hypoallergenic: pet_friendly: , noise: )
+dog_1.atts_table_id = atts_1.id 
+dog_1.save
+
+dog_1 = DogBreed.create(name: , origins: , life_span: , group: )
+atts_1 = AttsTable.create(dog_breed_id: dog_1.id, apartment_friendly: , hypoallergenic: pet_friendly: , noise: )
+dog_1.atts_table_id = atts_1.id 
+dog_1.save
+
+dog_1 = DogBreed.create(name: , origins: , life_span: , group: )
+atts_1 = AttsTable.create(dog_breed_id: dog_1.id, apartment_friendly: , hypoallergenic: pet_friendly: , noise: )
+dog_1.atts_table_id = atts_1.id 
+dog_1.save
+
+dog_1 = DogBreed.create(name: , origins: , life_span: , group: )
+atts_1 = AttsTable.create(dog_breed_id: dog_1.id, apartment_friendly: , hypoallergenic: pet_friendly: , noise: )
+dog_1.atts_table_id = atts_1.id 
+dog_1.save
+
+dog_1 = DogBreed.create(name: , origins: , life_span: , group: )
+atts_1 = AttsTable.create(dog_breed_id: dog_1.id, apartment_friendly: , hypoallergenic: pet_friendly: , noise: )
+dog_1.atts_table_id = atts_1.id 
+dog_1.save
+
+dog_1 = DogBreed.create(name: , origins: , life_span: , group: )
+atts_1 = AttsTable.create(dog_breed_id: dog_1.id, apartment_friendly: , hypoallergenic: pet_friendly: , noise: )
+dog_1.atts_table_id = atts_1.id 
+dog_1.save
+
+dog_1 = DogBreed.create(name: , origins: , life_span: , group: )
+atts_1 = AttsTable.create(dog_breed_id: dog_1.id, apartment_friendly: , hypoallergenic: pet_friendly: , noise: )
+dog_1.atts_table_id = atts_1.id 
+dog_1.save
+
+dog_1 = DogBreed.create(name: , origins: , life_span: , group: )
+atts_1 = AttsTable.create(dog_breed_id: dog_1.id, apartment_friendly: , hypoallergenic: pet_friendly: , noise: )
+dog_1.atts_table_id = atts_1.id 
+dog_1.save
