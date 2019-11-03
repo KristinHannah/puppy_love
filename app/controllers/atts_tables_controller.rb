e6e6ca139304a72da4070a99f45e554de812eac6
KristinHannah <kristinhannahk@gmail.com>
2019-11-03 12:00:29 -0500

    setting up attributes show

--- app/controllers/atts_tables_controller.rb
+++ app/controllers/atts_tables_controller.rb
@@ -2,11 +2,14 @@ class AttsTablesController < ApplicationController
     before_action :require_login
 
     def new 
-
+        @atts_table = AttsTable.new 
     end 
 
     def create 
-
+        @atts_table = AttsTable.create(atts_table_params)
+        @atts_table.user_id = session[:user_id]
+        @atts_table.save 
+        redirect_to atts_table_path(@atts_table)
     end 
 
     def show 
