88cc46b243609c402f31911af6de1007b04b2e9f
KristinHannah <kristinhannahk@gmail.com>
2019-11-04 21:07:41 -0500

    adding scope methods

--- app/models/atts_table.rb
+++ app/models/atts_table.rb
@@ -2,7 +2,9 @@ class AttsTable < ApplicationRecord
     belongs_to :dog_breed
     belongs_to :user 
 
-  #  scope :apartment_friendly, -> { where(apartment_friendly: true) }
-  #  scope :apartment_and_pet_friendly -> { apartment_friendly.where(pet_friendly: true) }
-  #  scope :hypoallergenic, -> { where(hypoallergenic: true) }
+    scope :apartment_friendly, -> { where(apartment_friendly: true) if dog_breed_id.present? }
+    scope :apartment_and_pet_friendly, -> { apartment_friendly.where(pet_friendly: true) }
+    scope :hypoallergenic, -> { where(hypoallergenic: true) if dog_breed_id.present? }
+    scope :no_noise, -> { where(noise: false) if dog_breed_id.present?}
 end
+
