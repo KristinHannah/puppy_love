56d06eae824a50788e17ed1217849040ea1cae25
KristinHannah <kristinhannahk@gmail.com>
2019-11-07 22:46:42 -0500

    added search bar

--- app/controllers/dog_breeds_controller.rb
+++ app/controllers/dog_breeds_controller.rb
@@ -1,10 +1,22 @@
 class DogBreedsController < ApplicationController
 
     def index
-        @dogs = DogBreed.all 
+        if params[:name]
+            @dogs = DogBreed.where('name LIKE ?', "%#{params[:name]}%")
+        else 
+          @dogs = DogBreed.all 
+        end
     end 
 
     def show 
         @dog = DogBreed.find(params[:id])
     end 
+
+    private
+
+    def dog_params 
+        params.require(:dog_breed).permit(:name)
+    end 
+
 end
+
