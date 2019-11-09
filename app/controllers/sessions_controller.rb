943e606b3a416f27bb370c832dc5bb30333ba699
KristinHannah <kristinhannahk@gmail.com>
2019-11-09 09:17:30 -0500

    fixed login issues

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,13 +14,13 @@ class SessionsController < ApplicationController
             redirect_to users_path
         else 
             #logged in normal
-         user = User.find_by(:email => params[:user][:email])
-            if user && user.authenticate(params[:user][:password])
+         user = User.find_by(:email => params[:email])
+            if user && user.authenticate(params[:password])
                 session[:user_id] = user.id 
 
-                redirect_to root_path
+                redirect_to users_path
             else 
-                render '/login'
+                render '/sessions/new'
             end 
          end 
     end 
