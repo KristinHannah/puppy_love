026dbdcca8a572cbd6b5e7783d16ce77f7176ddc
KristinHannah <kristinhannahk@gmail.com>
2019-11-09 12:31:07 -0500

    edited scope methods

--- app/models/atts_table.rb
+++ app/models/atts_table.rb
@@ -13,38 +13,42 @@ class AttsTable < ApplicationRecord
     scope :apartment_friendly, -> (answer) { where(:apartment_friendly => answer)}
     scope :by_size, -> (answer) { where(:size => answer)}
 
+    scope :pet_friendly, -> (answer) { where(:pet_friendly => answer) if answer == true}
+    scope :hypoallergenic, -> (answer) { where(:hypoallergenic => answer) if answer == true}
+    scope :no_noise, -> (answer) { where(:noise => answer) if answer == false}
+    scope :cold, -> (answer) { where(:cold_weather => answer) if answer == true}
 
-    def self.pet_friendly(answer)
-        if answer == true 
-            where(pet_friendly: true) 
-        else 
-            all
-        end 
-    end 
+   # def self.pet_friendly(answer)
+   #     if answer == true 
+   #         where(pet_friendly: true) 
+   #     else 
+   #         all
+   #     end 
+   # end 
 
-    def self.hypoallergenic(answer)
-        if answer == true 
-            where(hypoallergenic: true)
-        else 
-            all
-        end 
-    end 
+  #  def self.hypoallergenic(answer)
+   #     if answer == true 
+   #         where(hypoallergenic: true)
+   #     else 
+   #         all
+   #     end 
+   # end 
 
-    def self.no_noise(answer)
-        if answer == false
-            where(noise: false)
-        else 
-            all
-        end 
-    end 
+    # def self.no_noise(answer)
+    #    if answer == false
+    #        where(noise: false)
+    #    else 
+    #        all
+    #    end 
+    # end 
 
-    def self.cold(answer)
-        if answer == true 
-            where(cold_weather: true)
-        else 
-            all
-        end 
-    end 
+  #  def self.cold(answer)
+   #     if answer == true 
+    #        where(cold_weather: true)
+    #    else 
+    #        all
+    #    end 
+    # end 
 
 end
 
